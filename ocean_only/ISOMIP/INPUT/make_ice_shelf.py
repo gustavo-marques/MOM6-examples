294e4ddedb75e698adab8ca99cbd68febe79ded5
Gustavo Marques <gmarques@princeton.edu>
2016-05-27 12:01:19 -0400

    First attemp to use PBL
    Made small changes in the MOM_input (included PBL flag, but left it FALSE for now) and diag_table (reduced output freq. for prog.). I also modified make_ice_shelf.py (no longer interpolate the ice shelf parameters, just read every other values since we are going from dx=1km to dx=2km).

--- ocean_only/ISOMIP/INPUT/make_ice_shelf.py
+++ ocean_only/ISOMIP/INPUT/make_ice_shelf.py
@@ -8,48 +8,48 @@ import matplotlib.pyplot as plt
 
 
 # read provided netcdf
-x = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['x'][:]
-y = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['y'][:]
-upperSurface = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['upperSurface'][:]
-lowerSurface = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['lowerSurface'][:]
+x = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['x'][::2]
+y = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['y'][::2]
+upperSurface = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['upperSurface'][::2,::2]
+lowerSurface = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['lowerSurface'][::2,::2]
 [X,Y]=np.meshgrid(x/1.0e3,y/1.0e3)
 
 # MOM file
-icefile = netCDF4.Dataset('isomip_ice_shelf1.nc','r+')
+icefile = netCDF4.Dataset('isomip_ice_shelf3.nc','r+')
 #area = icefile.variables['area'][:] 
 #height = icefile.variables['height'][:] 
 
-x2 = x[::2] 
-y2 = y[::2] 
-[X2,Y2]=np.meshgrid(x2/1.0e3,y2/1.0e3)
+#x2 = x[::2] 
+#y2 = y[::2] 
+#[X2,Y2]=np.meshgrid(x2/1.0e3,y2/1.0e3)
 
-f = interpolate.interp2d(x, y, upperSurface, kind='linear')
-tmp_upper = f(x2,y2)
+#f = interpolate.interp2d(x, y, upperSurface, kind='linear')
+#tmp_upper = f(x2,y2)
 
 plt.figure()
-plt.contourf(X2,Y2,tmp_upper)
+plt.contourf(X,Y,upperSurface)
 plt.colorbar()
 plt.savefig('upperSurface.png')
 
-f = interpolate.interp2d(x, y, lowerSurface, kind='linear')
-tmp_lower = f(x2,y2)
+#f = interpolate.interp2d(x, y, lowerSurface, kind='linear')
+#tmp_lower = f(x2,y2)
 
 plt.figure()
-plt.contourf(X2,Y2,tmp_lower)
+plt.contourf(X,Y,lowerSurface)
 plt.colorbar()
 plt.savefig('lowerSurface.png')
 
-thick = tmp_upper - tmp_lower
-area = np.ones((thick.shape))* (x2[1]-x2[0]) * (y2[1]-y2[0])
+thick = upperSurface - lowerSurface
+area = np.ones((thick.shape))* (x[1]-x[0]) * (y[1]-y[0])
 area[thick==0.0]=0.0
 
 plt.figure()
-plt.contourf(X2,Y2,area)
+plt.contourf(X,Y,area)
 plt.colorbar()
 plt.savefig('area.png')
 
 plt.figure()
-plt.contourf(X2,Y2,thick)
+plt.contourf(X,Y,thick)
 plt.colorbar()
 plt.savefig('thick.png')
 
