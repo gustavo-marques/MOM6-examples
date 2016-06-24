531ebdb064931179913772d4c8029f5174fbbf28
Gustavo Marques <gmarques@princeton.edu>
2016-06-24 09:48:19 -0400

    Some clean up

--- ocean_only/ISOMIP/2D/INPUT/make_ice_shelf.py
+++ ocean_only/ISOMIP/2D/INPUT/make_ice_shelf.py
@@ -2,10 +2,8 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 import netCDF4
-from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 # read provided netcdf
 x = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['x'][::2]
@@ -15,20 +13,25 @@ lowerSurface = netCDF4.Dataset('Ocean1_input_geom_v1.01.nc').variables['lowerSur
 [X,Y]=np.meshgrid(x/1.0e3,y/1.0e3)
 
 # MOM file
-icefile = netCDF4.Dataset('2D_IC_zeros.nc','r+')
+#icefile = netCDF4.Dataset('2D_IC.nc','r+')
+icefile = netCDF4.Dataset('2D_IC_half_end.nc','r+')
 
 thick = upperSurface - lowerSurface
 area = np.ones((thick.shape))* (x[1]-x[0]) * (y[1]-y[0])
 area[thick==0.0]=0.0
 
+# reduce the thickness of the shelf front in half
+thick[:,159]=0.5*thick[:,159]
+lowerSurface[:,159]=0.5*lowerSurface[:,159]
+
 icefile.variables['area'][0,:] = area[20,:]
 icefile.variables['area'][1,:] = area[20,:]
 
-icefile.variables['thick'][0,:] = 0.0 #thick[20,:]
-icefile.variables['thick'][1,:] = 0.0 #thick[20,:]
+icefile.variables['thick'][0,:] = thick[20,:]
+icefile.variables['thick'][1,:] = thick[20,:]
 
-icefile.variables['height'][0,:] = 0.0 #lowerSurface[20,:]
-icefile.variables['height'][1,:] = 0.0 #lowerSurface[20,:]
+icefile.variables['height'][0,:] = lowerSurface[20,:]
+icefile.variables['height'][1,:] = lowerSurface[20,:]
 
 icefile.close()
 print 'Done!'
